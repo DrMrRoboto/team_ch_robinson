bd99f76a883e1f26ac81a92b8cd7f39814c193c3
Totep <declarationofMutiny@hotmail.com>
2016-01-21 11:14:45 -0600

    event schema creation

---
+++ server/models/events.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Totep on 1/21/16.
+ */
+var mongoose = require('mongoose');
+var Tasks = require('tasks');
+var Shifts = require('shifts');
+var Volunteers = require('volunteers');
+
+//creats a schema to standardize Event creation
+
+var EventSchema = new mongoose.Schema ({
+    name: String,
+    description: String,
+    startDate: Date,
+    endDate: Date,
+    startTime: ,
+    endTime: ,
+    host: String
+});
+
+//Deletes Event and all associated Categories and Items
+//
+//EventSchema.statics.delete = function(id, callback){
+//    //Deletes the Event using the Event id
+//    this.findById(id, function(err, data){
+//        if(err) {
+//            callback(err);
+//        } else if (result != undefined){
+//
+//        }
+//    })
+//}
+
+module.exports = mongoose.model('Event', EventSchema);
