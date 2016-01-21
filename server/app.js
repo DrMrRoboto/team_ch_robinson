4e61c9a754089aafb8a16d200252f9df14ed2bb7
Clayton Hottinger <chottinger@Claytons-MacBook-Pro.local>
2016-01-21 14:25:06 -0600

    basic angular routing set up with placeholder templates

--- server/app.js
+++ server/app.js
@@ -40,7 +40,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../client/private')));
+app.use(express.static(path.join(__dirname, '../client/public')));
 
 app.use('/', routes);
 app.use('/users', users);
