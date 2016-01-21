97f93f63bdfe4e7f4f3546e6a3386b099dfab613
Totep <declarationofMutiny@hotmail.com>
2016-01-21 13:58:56 -0600

    tasks schema with requirements and delete

--- server/models/tasks.js
+++ server/models/tasks.js
@@ -12,7 +12,25 @@ var TaskSchema = new mongoose.Schema ({
     name: {String, required: true},
     description: {String, required: true},
     event_id: {String, required: true}
-
 });
+
+// Deletes Task and all associated Items
+
+TaskSchema.statics.delete = function(id, callback){
+    this.findById(id, function(err, result){
+        if (err) {
+            callback(err);
+        } else if (result != undefined){
+            result.remove(callback);
+        }
+    });
+    Shift.remove({categoryID: id}, function(err){
+        if(err) {
+            callback(err);
+        } else if (!err); {
+            callback()
+        }
+    })
+};
 
 module.exports = mongoose.model('Task', TaskSchema);
