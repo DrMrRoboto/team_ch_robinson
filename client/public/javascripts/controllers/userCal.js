029389ee1ae9ceb0ce7751d5e1a2b34224992993
Clayton Hottinger <funkybarracuda@gmail.com>
2016-01-22 16:25:06 -0600

    Basic calendar display is up and running, customization and interaction with the database next

--- client/public/javascripts/controllers/userCal.js
+++ client/public/javascripts/controllers/userCal.js
@@ -1,6 +1,19 @@
 /**
  * Created by chottinger on 1/21/16.
  */
-app.controller('userCal', function(){
+app.controller('userCal', ['$scope','moment',function($scope, moment){
+  $scope.eventData = [
+    {
+      title: 'Bring your children as slaves day',
+      type: 'info',
+      startsAt: moment().startOf('month').toDate()
+    }
+  ];
 
-});
+  $scope.view = 'month';
+
+  $scope.viewDate = moment().startOf('month').toDate();
+
+  console.log($scope.viewDate);
+
+}]);
