25d33b85bcb326780b237c7cb5f4c27c30594191
BrotherSeamus <jamespkirwin@gmail.com>
2016-01-22 15:41:21 -0600

    created static elements of volunteerList.html

--- client/public/javascripts/controllers/volunteerList.js
+++ client/public/javascripts/controllers/volunteerList.js
@@ -1,6 +1,10 @@
 /**
  * Created by chottinger on 1/21/16.
  */
-app.controller('volunteerList', function(){
+app.controller('volunteerList', ["$scope", function($scope){
 
-});
+	$scope.event = {
+		name: "Test Event"
+	};
+
+}]);
