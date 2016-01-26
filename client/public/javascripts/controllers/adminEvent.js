305dbba9e2143606d8a99527bfe53d9dc115b004
DrMrRoboto <margis.ben@gmail.com>
2016-01-26 15:15:13 -0600

    Added datetime picker to adminEvent

--- client/public/javascripts/controllers/adminEvent.js
+++ client/public/javascripts/controllers/adminEvent.js
@@ -1,6 +1,30 @@
 /**
  * Created by chottinger on 1/21/16.
  */
-app.controller('adminEvent',function(){
+app.controller('adminEvent',['$scope',function($scope){
 
-});
+    $scope.mydate='';
+
+    $scope.isOpen = false;
+
+    $scope.openCalendar = function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        $scope.isOpen = true;
+    };
+
+}]);
+
+//app.controller('adminEvent',['$scope',function($scope){
+//
+//    $scope.isOpen = false;
+//
+//    $scope.openCalendar = function(e) {
+//        e.preventDefault();
+//        e.stopPropagation();
+//
+//        $scope.isOpen = true;
+//    };
+//
+//}]);
