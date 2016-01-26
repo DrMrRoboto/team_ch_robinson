d09d3091b0aa7b6dc937da303d5c400edd032c9b
Clayton Hottinger <chottinger@Claytons-MacBook-Pro.local>
2016-01-26 16:34:11 -0600

    All events can be gotten in user and admin cal views, huzzah exclamation point

--- client/public/javascripts/controllers/userCal.js
+++ client/public/javascripts/controllers/userCal.js
@@ -1,8 +1,14 @@
 /**
+ *
  * Created by chottinger on 1/21/16.
  */
-app.controller('userCal', ['$scope','moment', 'calendarConfig',
-  function($scope, moment, calendarConfig, eventAjax){
+app.controller('userCal', ['$scope','moment', 'calendarConfig', 'eventServe',
+  function($scope, moment, calendarConfig, eventServe){
+
+  //sets eventData, but only after $http call in getEvents() is complete
+  eventServe.getEvents().then(function(response){
+    $scope.eventData = response;
+  });
 
   var monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
     'July', 'August', 'September', 'October', 'November', 'December'
@@ -10,40 +16,9 @@ app.controller('userCal', ['$scope','moment', 'calendarConfig',
 
   calendarConfig.templates.calendarMonthCell = 'views/templates/test.html';
 
-  $scope.eventData = [
-    {
-      title: 'Event ID 1',
-      type: 'info',
-      startsAt: new Date(2016, 1, 12, 15),
-      endsAt: new Date(2016, 1, 12, 16),
-      id: 1
-    },
-    {
-      title: 'Event ID 2',
-      type: 'info',
-      startsAt: new Date(2016, 1, 12, 15),
-      endsAt: new Date(2016, 1, 12, 16),
-      id: 2
-    },
-    {
-      title: 'Event ID 3',
-      type: 'info',
-      startsAt: new Date(2016, 1, 12, 15),
-      endsAt: new Date(2016, 1, 12, 16),
-      id: 3
-    },
-    {
-      title: 'Event ID 4',
-      type: 'info',
-      startsAt: new Date(2016, 1, 12, 15),
-      endsAt: new Date(2016, 1, 12, 16),
-      id: 4
-    }
-  ];
-
   $scope.view = 'month';
 
-  $scope.viewDate = new Date(2016, 1, 1, 0);
+  $scope.viewDate = new Date(2016, 0, 1, 0);
 
   $scope.viewTitle = monthNames[$scope.viewDate.getMonth()];
 
