e2432dc9e0b79cd95a79eb4191a52fc9c8998128
Totep <declarationofMutiny@hotmail.com>
2016-01-26 09:27:27 -0600

    rolled over userCal to adminCal

--- client/public/javascripts/controllers/adminCal.js
+++ client/public/javascripts/controllers/adminCal.js
@@ -1,6 +1,50 @@
 /**
  * Created by chottinger on 1/21/16.
  */
-app.controller('adminCal',function(){
+app.controller('adminCal', ['$scope','moment', 'calendarConfig',function($scope, moment, calendarConfig){
 
-});
+    var monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December'
+    ];
+
+    calendarConfig.templates.calendarMonthCell = 'views/templates/test.html';
+
+    $scope.eventData = [
+        {
+            title: 'Event ID 1',
+            type: 'info',
+            startsAt: new Date(2016, 1, 12, 15),
+            endsAt: new Date(2016, 1, 12, 16),
+            id: 1
+        },
+        {
+            title: 'Event ID 2',
+            type: 'info',
+            startsAt: new Date(2016, 1, 12, 15),
+            endsAt: new Date(2016, 1, 12, 16),
+            id: 2
+        },
+        {
+            title: 'Event ID 3',
+            type: 'info',
+            startsAt: new Date(2016, 1, 12, 15),
+            endsAt: new Date(2016, 1, 12, 16),
+            id: 3
+        },
+        {
+            title: 'Event ID 4',
+            type: 'info',
+            startsAt: new Date(2016, 1, 12, 15),
+            endsAt: new Date(2016, 1, 12, 16),
+            id: 4
+        }
+    ];
+
+    $scope.view = 'month';
+
+    $scope.viewDate = new Date(2016, 1, 1, 0);
+
+    $scope.viewTitle = monthNames[$scope.viewDate.getMonth()];
+
+
+}]);
