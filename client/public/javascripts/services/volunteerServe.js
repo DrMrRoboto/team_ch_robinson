ec092f6e9000af4f524d68e926e5c7b1fa5389fa
Totep <declarationofMutiny@hotmail.com>
2016-01-27 09:57:58 -0600

    volunteerServe skeleton

---
+++ client/public/javascripts/services/volunteerServe.js
@@ -0,0 +1,92 @@
+/**
+ * Created by Totep on 1/27/16.
+ */
+app.factory('volunteerServe',['$http',function($http){
+    return {
+
+        /**
+         * Gets single volunteer from database
+         * @param volunteerId
+         * @returns $http promise object
+         */
+        getVolunteer: function(volunteerId){
+            return $http({
+                method: 'get',
+                url: '/volunteers/' + volunteerId
+            }).then(function(response){
+                return response.data
+            }, function(errorResponse){
+                return errorResponse.data
+            });
+        },
+        /**
+         * Gets all volunteers from database, adds necessary fields to data before
+         * @returns $http promise object
+         */
+        getVolunteers: function(){
+            return $http({
+                method: 'get',
+                url: '/volunteers'
+            }).then(function(response){
+                var data = [];
+                response.data.forEach(function(element){
+                    element.type = 'info';
+                    data.push(element);
+                });
+                return response.data;
+            }, function(errorResponse){
+                return errorResponse.data
+            });
+        },
+
+        /**
+         * Sends new volunteer to be posted to the database
+         * @param newVolunteer
+         * @returns $http promise object
+         */
+
+        postVolunteer: function(){
+            return $http({
+                method: 'post',
+                url: '/volunteers/' + volunteerId
+            }).then(function(response){
+                return response.data
+            }, function(errorResponse){
+                return errorResponse.data
+            });
+        },
+        /**
+        * Sends volunteerId to be updated in database, along with new event to replace current data
+        * @param volunteerId
+        * @param updatedVolunteer
+        * @returns $http promise object
+        */
+        updateVolunteer: function(volunteerId, updatedVolunteer){
+            return $http({
+                method: 'put',
+                data: updatedVolunteer,
+                url: '/volunteers/' + volunteerId
+            }).then(function(response){
+                return response.data
+            }, function(errorResponse){
+                return errorResponse.data
+            });
+        },
+        /**
+         * Sends volunteerId to be deleted in database
+         * @param volunteerId
+         * @returns $http promise object
+         */
+        deleteVolunteer: function(volunteerId){
+            return $http({
+                method: 'delete',
+                url: '/volunteers/' + volunteerId
+            }).then(function(response){
+                return response.data
+            }, function(errorResponse){
+                return errorResponse.data
+            });
+        }
+
+}
+}]);
