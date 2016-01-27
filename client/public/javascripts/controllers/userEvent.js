39dd9e9b4d517a82ce1fca660234c9089c02e21d
Totep <declarationofMutiny@hotmail.com>
2016-01-27 11:55:47 -0600

    fixed mistake

--- client/public/javascripts/controllers/userEvent.js
+++ client/public/javascripts/controllers/userEvent.js
@@ -3,26 +3,4 @@
  */
 app.controller('userEvent', ['$scope', '$http', function ($scope, $http) {
 
-    $scope.volunteer = {};
-    $scope.volunteers = [];
-    var fetchVolunteers = function() {
-        return $http.get('/userEvent').then(function(response){
-            if(response.status !== 200){
-                throw new Error('Failed');
-            }
-            $scope.volunteer = {};
-            $scope.volunteers = response.data;
-            return response.data;
-        })
-    };
-
-    $scope.insert = function(newVolunteer) {
-        $http({
-            url: '/userEvent',
-            method: 'post',
-            data: volunteer
-        }).then(function(response){
-            $scope.volunteer = {};
-        });
-    };
 }]);
