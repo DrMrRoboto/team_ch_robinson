299c65d671af66637618da201289a8253b525f52
Clayton Hottinger <funkybarracuda@gmail.com>
2016-01-27 15:42:47 -0600

    ng-click on save button

--- client/public/javascripts/controllers/userEvent.js
+++ client/public/javascripts/controllers/userEvent.js
@@ -1,6 +1,10 @@
 /**
  * Created by chottinger on 1/21/16.
  */
-app.controller('userEvent', ['$scope', '$http', function ($scope, $http) {
+app.controller('userEvent', ['$scope', function ($scope, $http) {
+
+  $scope.saveEvent = function(){
+
+  }
 
 }]);
