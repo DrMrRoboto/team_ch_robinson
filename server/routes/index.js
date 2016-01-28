11a0aa224edbf0344d58a6475d3bbb688f2f7ccb
BrotherSeamus <jamespkirwin@gmail.com>
2016-01-28 09:59:45 -0600

    updated application title

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CH Robinson joinUP' });
+  res.render('index', { title: 'CHR Volunteers' });
 });
 
 module.exports = router;
