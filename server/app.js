91656ee2fc7fb37adc2bc481b74f735e4469a1b3
Clayton Hottinger <chottinger@Claytons-MacBook-Pro.local>
2016-01-29 13:52:56 -0600

    moved jade files back to server folder, added datetime-rangepicker files to vendors folder

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ var volunteers = require('./routes/volunteers');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, '../client/public/views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 //DATABASE CONNECTIONS
