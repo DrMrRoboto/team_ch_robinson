7789fb6558cca6797a3ef7d9bbfc843bf116de68
Clayton Hottinger <chottinger@Claytons-MacBook-Pro.local>
2016-02-01 16:23:58 -0600

    Beginnings of the volunteer report

--- client/public/javascripts/controllers/volunteerList.js
+++ client/public/javascripts/controllers/volunteerList.js
@@ -4,23 +4,57 @@
 app.controller('volunteerList', ['$scope','$routeParams','eventServe','taskServe','shiftServe','volunteerServe',
 	function($scope, $routeParams, eventServe, taskServe, shiftServe, volunteerServe){
 
+	$scope.data = {};
+
+	$scope.displayData = [];
+
 	eventServe.getEvent($routeParams.id).then(function(response){
 		$scope.data.eventName = response.title;
-
 	});
 
-	taskServe.getTasks($routeParams.id).then(function(response){
+	taskServe.getTasks($routeParams.id).then(function(response) {
 		$scope.data.tasks = response;
-		$scope.data.tasks.forEach(function(element){
-			shiftServe.getShifts(element._id).then(function(response){
+		$scope.data.tasks.forEach(function (element) {
+			shiftServe.getShifts(element._id).then(function (response) {
 				element.shifts = response;
-				element.shifts.forEach(function(element){
-					volunteerServe.getVolunteers(element._id).then(function(response){
-						element.volunteers = response;
+				element.shifts.forEach(function (element) {
+					volunteerServe.getVolunteers(element._id).then(function (response) {
+						for (var i = 0; i < element.slotsAvailable; i++) {
+							if (response[i]) {
+								var newTableObject = {
+									taskName: element.taskName,
+									startTime: element.startTime,
+									endTime: element.endTime,
+									volunteerName: response[i].firstName + ' ' + response[i].lastName,
+									volunteerEmail: response[i].email,
+									volunteerPhone: response[i].phone,
+									volunteerShirt: response[i].shirtSize,
+									volunteerGuests: response[i].guests
+								};
+							} else {
+								var newTableObject = {
+									taskName: element.taskName,
+									startTime: element.startTime,
+									endTime: element.endTime,
+									volunteerName: '',
+									volunteerEmail: '',
+									volunteerPhone: '',
+									volunteerShirt: '',
+									volunteerGuests: []
+								}
+
+							}
+							$scope.displayData.push(newTableObject);
+							element.volunteers = response;
+						}
 					});
 				});
 			});
 		});
 	});
+
+	setTimeout(function(){
+		console.log($scope.data);
+	}, 3000);
 
 }]);
