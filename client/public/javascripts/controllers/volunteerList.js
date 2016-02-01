ba6e38a81625f18d4235a090a8d6c530164df13d
Clayton Hottinger <funkybarracuda@gmail.com>
2016-02-01 10:57:34 -0600

    slowly but surely, volunteer list is being built

--- client/public/javascripts/controllers/volunteerList.js
+++ client/public/javascripts/controllers/volunteerList.js
@@ -1,13 +1,26 @@
 /**
  * Created by chottinger on 1/21/16.
  */
-app.controller('volunteerList', ['$scope','eventServe','taskServe','shiftServe','volunteerServe',
-	function($scope, eventServe, taskServe, shiftServe, volunteerServe){
+app.controller('volunteerList', ['$scope','$routeParams','eventServe','taskServe','shiftServe','volunteerServe',
+	function($scope, $routeParams, eventServe, taskServe, shiftServe, volunteerServe){
 
+	eventServe.getEvent($routeParams.id).then(function(response){
+		$scope.data.eventName = response.title;
 
+	});
 
-	$scope.event = {
-		name: "Test Event"
-	};
+	taskServe.getTasks($routeParams.id).then(function(response){
+		$scope.data.tasks = response;
+		$scope.data.tasks.forEach(function(element){
+			shiftServe.getShifts(element._id).then(function(response){
+				element.shifts = response;
+				element.shifts.forEach(function(element){
+					volunteerServe.getVolunteers(element._id).then(function(response){
+						element.volunteers = response;
+					});
+				});
+			});
+		});
+	});
 
 }]);
