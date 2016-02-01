facd0daea5db4edc57a5dc02236d1efd582f7854
BrotherSeamus <jamespkirwin@gmail.com>
2016-02-01 15:10:32 -0600

    Now able to create shifts in adminEvent view

--- client/public/javascripts/controllers/adminEvent.js
+++ client/public/javascripts/controllers/adminEvent.js
@@ -4,8 +4,15 @@
 app.controller('adminEvent',['$scope','$routeParams','eventServe', 'taskServe', 'shiftServe',
     function($scope, $routeParams, eventServe, taskServe, shiftServe){
 
+    /**
+     * Array to hold list of Tasks for a specific Event ID
+     * @type {Array}
+   */
     $scope.tasks = [];
 
+  /**
+   * If an event ID is passed to the URL. This pulls down the information for a specific Event
+   */
     if($routeParams.id){
         eventServe.getEvent($routeParams.id).then(function(response){
             $scope.event = response;
@@ -14,8 +21,8 @@ app.controller('adminEvent',['$scope','$routeParams','eventServe', 'taskServe', 
                 $scope.tasks = response;
                 $scope.tasks.forEach(function(element){
                     element.slider = {
-                        min: 0,
-                        max: 1440,
+                        min: 480,
+                        max: 1020,
                         options: {
                             floor:0,
                             ceil:1440,
@@ -45,6 +52,7 @@ app.controller('adminEvent',['$scope','$routeParams','eventServe', 'taskServe', 
 
     $scope.startAtOpen = false;
     $scope.endAtOpen = false;
+    $scope.shiftDateOpen = false;
 
     $scope.openStartAt = function(e) {
         e.preventDefault();
@@ -57,6 +65,12 @@ app.controller('adminEvent',['$scope','$routeParams','eventServe', 'taskServe', 
         e.stopPropagation();
 
         $scope.endAtOpen = true;
+    };
+    $scope.openShiftDate= function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        $scope.shiftDateOpen = true;
     };
 
     $scope.saveEvent = function (){
@@ -95,21 +109,37 @@ app.controller('adminEvent',['$scope','$routeParams','eventServe', 'taskServe', 
         $scope.newTask.description = '';
     };
 
-    $scope.tasks.slider = {
-        min: 0,
-        max: 1440,
-        options: {
-            floor:0,
-            ceil:1440,
-            step: 30,
-            draggableRange: true,
-            minRange: 30,
-            showSelectionBar: true,
-            showSelectionBarEnd: true,
-            hideLimitLabels: true
-        }
+
+    $scope.newShift = {
+        date: "",
+        startTime: "",
+        endTime: "",
+        slotsAvailable: "",
+        task_id: "",
+        task_name: ""
     };
 
+        $scope.shiftLength = "";
+        $scope.shiftSlots = "";
+        $scope.shiftDate = "";
 
+    $scope.saveNewShift = function(taskId, taskName, index, shiftLength, shiftSlots, shiftDate) {
+
+        var start = $scope.tasks[index].slider.min;
+        var end = $scope.tasks[index].slider.max;
+        var length = parseInt(shiftLength);
+
+        $scope.newShift.date = shiftDate;
+        $scope.newShift.slotsAvailable = shiftSlots;
+        $scope.newShift.task_id = taskId;
+        $scope.newShift.task_name = taskName;
+
+        for(var i = 0; i<(end-start)/length; i++) {
+            $scope.newShift.startTime = start + (i * length);
+            $scope.newShift.endTime = $scope.newShift.startTime + length;
+            console.log($scope.newShift);
+            shiftServe.createShift(angular.copy($scope.newShift));
+        }
+    };
 }]);
 
