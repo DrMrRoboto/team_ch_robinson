9350d5fea0e670c0f1bf2457d511182cea0c6411
Clayton Hottinger <chottinger@Claytons-MacBook-Pro.local>
2016-02-01 09:56:16 -0600

    started work on volunteer report logic

--- client/public/javascripts/controllers/volunteerList.js
+++ client/public/javascripts/controllers/volunteerList.js
@@ -1,7 +1,10 @@
 /**
  * Created by chottinger on 1/21/16.
  */
-app.controller('volunteerList', ["$scope", function($scope){
+app.controller('volunteerList', ['$scope','eventServe','taskServe','shiftServe','volunteerServe',
+	function($scope, eventServe, taskServe, shiftServe, volunteerServe){
+
+
 
 	$scope.event = {
 		name: "Test Event"
