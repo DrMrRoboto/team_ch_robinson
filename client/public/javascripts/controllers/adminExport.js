53e72bda1d448f3c388f61ac5d83af971b1c60a0
Totep <declarationofMutiny@hotmail.com>
2016-02-01 15:26:16 -0600

    partial export data

---
+++ client/public/javascripts/controllers/adminExport.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Totep on 2/1/16.
+ */
--- client/public/javascripts/controllers/userCal.js
+++ client/public/javascripts/controllers/userCal.js
@@ -19,10 +19,13 @@ app.controller('userCal', ['$scope','moment', 'calendarConfig', 'eventServe',
 
   $scope.view = 'month';
 
+    //sets calendarView to current month
   $scope.viewDate = new Date(moment());
 
   $scope.viewTitle = monthNames[$scope.viewDate.getMonth()];
 
+    //$scope.now is called within admin/user calendarView pages so that upcoming
+    //and previous events are displayed within the context of 'today'
   $scope.now = moment();
 
 }]);
