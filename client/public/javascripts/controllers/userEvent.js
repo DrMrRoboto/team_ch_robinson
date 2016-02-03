b897eabce0c120653aad72bd50d1b549e871a676
BrotherSeamus <jamespkirwin@gmail.com>
2016-02-03 11:20:17 -0600

    created user volunteer sign up and made form required

--- client/public/javascripts/controllers/userEvent.js
+++ client/public/javascripts/controllers/userEvent.js
@@ -1,19 +1,77 @@
 /**
  * Created by chottinger on 1/21/16.
  */
-app.controller('userEvent', ['$scope', '$routeParams', 'eventServe', 'shiftServe',
-	function ($scope, $routeParams, eventServe, shiftServe) {
+app.controller('userEvent', ['$scope', '$routeParams', 'eventServe', 'taskServe', 'shiftServe', 'volunteerServe',
+	function ($scope, $routeParams, eventServe, taskServe, shiftServe, volunteerServe) {
 
 		$scope.shifts = [];
+		$scope.newVolunteer = {
+			firstName: "",
+			lastName: "",
+			email: "",
+			phone: "",
+			shirtSize: "",
+			guests: ""
+		};
+		$scope.newGuest = {
+			name: "",
+			shirtSize: ""
+		};
+		$scope.guests = [];
 
-		if ($routeParams.id) {
+		$scope.loadUserEvent = function() {
 			eventServe.getEvent($routeParams.id).then(function (response) {
 				$scope.event = response;
-				shiftServe.getShifts($routeParams.id).then(function (response) {
-					console.log(response);
-					$scope.shifts = response;
-				});
 			});
+			taskServe.getTasks($routeParams.id).then(function(response) {
+				$scope.tasks = response;
+				$scope.tasks.forEach(function(task) {
+					shiftServe.getShifts(task._id).then(function (response) {
+						$scope.shiftResponse = response;
+						$scope.shiftResponse.forEach(function(shift){
+							volunteerServe.getVolunteers(shift._id).then(function(response) {
+								shift.volunteers = response;
+								$scope.shifts.push(shift);
+							});
+						});
+					});
+				})
+			});
+		};
+
+		if ($routeParams.id) {
+			$scope.loadUserEvent();
 		}
 
+		$scope.clearGuests = function() {
+			$scope.guests = [];
+		};
+
+		$scope.addGuest = function() {
+			var guest = $scope.newGuest.name + " - " + $scope.newGuest.shirtSize;
+			$scope.guests.push(guest);
+			$scope.newGuest = {
+				name: "",
+				shirtSize: ""
+			};
+		};
+
+		$scope.saveVolunteer = function(shiftId) {
+			$scope.shifts = [];
+			$scope.newVolunteer.shift_id = shiftId;
+			$scope.newVolunteer.guests = $scope.guests;
+			volunteerServe.postVolunteer($scope.newVolunteer)
+					.then(function(){
+				$scope.loadUserEvent();
+				$scope.newVolunteer = {
+					firstName: "",
+					lastName: "",
+					email: "",
+					phone: "",
+					shirtSize: "",
+					guests: ""
+				};
+				$scope.clearGuests();
+			});
+		}
 	}]);
