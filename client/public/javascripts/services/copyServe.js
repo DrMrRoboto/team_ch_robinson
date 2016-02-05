92756c8507d5abcaf75d21af1d70e99c6dbac890
BrotherSeamus <jamespkirwin@gmail.com>
2016-02-05 17:46:58 -0600

    I have no idea what I am doing

--- client/public/javascripts/services/copyServe.js
+++ client/public/javascripts/services/copyServe.js
@@ -1,9 +1,14 @@
-app.factory('copyServe',['$http', 'eventServe', 'taskServe', 'shiftServe',
-	function($http, eventServe, taskServe, shiftServe){
+app.factory('copyServe',['$http', 'eventServe', 'taskServe',
+	function($http, eventServe, taskServe){
 
 		return {
 			copyEvent: function(eventId, newStartAt, newEndAt) {
-
+				eventServe.getEvent(eventId).then(function(res){
+					var newEvent = res;
+					newEvent.startsAt = newStartAt;
+					newEvent.endsAt = newEndAt;
+					eventServe.createEvent(newEvent);
+				})
 			}
 		};
 
