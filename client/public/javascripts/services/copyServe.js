a7c9cea4117a4c1432bb245376e23a4f6ee2379a
BrotherSeamus <jamespkirwin@gmail.com>
2016-02-05 16:53:57 -0600

    started creating EvenCopy function

---
+++ client/public/javascripts/services/copyServe.js
@@ -0,0 +1,10 @@
+app.factory('copyServe',['$http', 'eventServe', 'taskServe', 'shiftServe',
+	function($http, eventServe, taskServe, shiftServe){
+
+		return {
+			copyEvent: function(eventId, newStartAt, newEndAt) {
+
+			}
+		};
+
+	}]);
